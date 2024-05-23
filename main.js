dc593dcd351203be8c25fd80f2c2c52accdc5195
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-23 03:01:38 +0300

    complete eventlistener to open and close mobile menu and add active state to navigation

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-import './style.css'
+import "./style.css";
 
+const hamburgerBtn = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".navBar");
+const mobileMenu = document.querySelector(".MenuModal");
+let isMenuOpen = false;
+
+const HandleOpenMenu = () => {
+  // opening menu
+  if (!isMenuOpen) {
+    menu.classList.remove("hidden");
+    mobileMenu.classList.add("active");
+    hamburgerBtn.classList.add("inactive");
+    isMenuOpen = true;
+  }
+};
+const handleCloseMenu = () => {
+  menu.classList.add("hidden");
+  mobileMenu.classList.remove("active");
+  hamburgerBtn.classList.remove("inactive");
+  isMenuOpen = false;
+};
+// hamburgerBtn.addEventListener("click", HandleOpenMenu);
+
+document.addEventListener("click", (e) => {
+  let clickInside = mobileMenu && mobileMenu.contains(e.target);
+  if (!isMenuOpen) {
+    hamburgerBtn.contains(e.target) && HandleOpenMenu();
+  } else {
+    // if clicked outside of mobile menu close menu modal
+    if (!clickInside) {
+      handleCloseMenu();
+    }
+  }
+});
